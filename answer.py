0d07f86979d6106d62bd0c9aba5d3389b51d7ad2
christiankuhnn <kuhnc@udel.edu>
2023-04-27 18:06:56 -0400

    devouring doggos

--- answer.py
+++ answer.py
@@ -1,4 +1,15 @@
-def knapsack(items, capacity):
+def knapsack(filename):
+    with open(filename, 'r') as f:
+        n_items = int(f.readline())
+        capacity = int(f.readline())
+        items = []
+        for i in range(n_items):
+            line = f.readline().split()
+            item_name = line[0]
+            item_weight = int(line[1])
+            item_value = int(line[2])
+            items.append((item_name, item_weight, item_value))
+
     n_items = len(items)
     table = [[0 for _ in range(capacity + 1)] for _ in range(n_items + 1)]
 
@@ -19,19 +30,15 @@ def knapsack(items, capacity):
             selected_items.append(item_name)
             w -= item_weight
 
+    # Return the selected items and their total value as a tuple
+    return (selected_items, table[n_items][capacity])
+
+
     # Print the result
     for item in reversed(selected_items):
         print(item)
     print(table[n_items][capacity])
 
-if __name__ == '__main__':
-    n_items = int(input())
-    capacity = int(input())
-    items = []
-    for _ in range(n_items):
-        line = input().split()
-        item_name = line[0]
-        item_weight = int(line[1])
-        item_value = int(line[2])
-        items.append((item_name, item_weight, item_value))
-    knapsack(items, capacity)
+if __name__ == "__main__":
+    filename = input()
+    knapsack(filename)
--- test.py
+++ test.py
@@ -1,26 +1,10 @@
-from io import StringIO
-import sys
 import unittest
 from answer import knapsack
 
-class TestKnapsack(unittest.TestCase):
-    def test_knapsack(self):
-        input_str = '5\n15\nTextbook 4 50\nHardDrive 10 2\nDogFood 10 5\nFavoriteGame 20 60\nSuperComputer 100 100\n'
-        expected_output_str = 'Textbook\nDogFood\n55\n'
+class KnapsackTest(unittest.TestCase):
+    def test_find_optimal_items(self):
+        # Test with given input
+        self.assertEqual(knapsack('items.txt'), (['DogFood', 'Textbook'], 55))
 
-        # Redirect standard input and output to StringIO objects
-        sys.stdin = StringIO(input_str)
-        sys.stdout = output_str = StringIO()
-
-        # Call the knapsack function
-        knapsack()
-
-        # Get the actual output from the StringIO object
-        actual_output_str = output_str.getvalue()
-
-        # Restore standard input and output
-        sys.stdin = sys.__stdin__
-        sys.stdout = sys.__stdout__
-
-        # Compare the actual output with the expected output
-        self.assertEqual(actual_output_str, expected_output_str)
+if __name__ == '__main__':
+    unittest.main()
